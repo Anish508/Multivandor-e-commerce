0f273f8093357e076e6180d4baf372443f377678
Anish508 <anishbarke9741@gmail.com>
2025-11-11 18:36:21 +0530

    feat: add Redis integration and dotenv support
    - Updated package.json to include new dependencies: dotenv, ejs, ioredis, nodemailer, and redis.
    - Created a new Redis client in packages/libs/redis/index.ts with configuration from environment variables.
    - Added a backup configuration file for Prisma with database connection settings.

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { sendOtp, validateRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+import { checkOptRestrictions, trackOtpRequests } from "../utils/auth.helper";
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
+
+    const { name, email } = req.body;
+    const existingUser = await prisma.users.findUnique({ where: email });
+
+    if (existingUser) {
+      return next(new ValidationError("User already exists with this email"));
+    }
+
+    await checkOptRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(email, name, "user-activation-mail");
+
+    res.status(200).json({
+      message: "Otp sent to email. Please verify your account.",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
